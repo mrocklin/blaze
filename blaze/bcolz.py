c3ccb52c4813357d2eba14db1f679ec43b417429
Matthew Rocklin <mrocklin@gmail.com>
2014-08-07 13:20:37 -0700

    bcolz: avoid amgibuities in into

--- blaze/bcolz.py
+++ blaze/bcolz.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import bcolz
+from bcolz import carray, ctable
 import numpy as np
 from pandas import DataFrame
 from collections import Iterator
@@ -13,29 +14,29 @@ from .compute.bcolz import *
 __all__ = ['into']
 
 
-@dispatch(type, (bcolz.ctable, bcolz.carray))
+@dispatch(type, (ctable, carray))
 def into(a, b, **kwargs):
     f = into.resolve((a, type(b)))
     return f(a, b, **kwargs)
 
-@dispatch((tuple, set, list, object), (bcolz.ctable, bcolz.carray))
+@dispatch((tuple, set, list), (ctable, carray))
 def into(o, b):
     return into(o, into(np.ndarray(0), b))
 
 
-@dispatch(np.ndarray, (bcolz.ctable, bcolz.carray))
+@dispatch(np.ndarray, (ctable, carray))
 def into(a, b):
     return b[:]
 
 
-@dispatch(bcolz.ctable, np.ndarray)
+@dispatch(ctable, np.ndarray)
 def into(a, b, **kwargs):
-    return bcolz.ctable(b, **kwargs)
+    return ctable(b, **kwargs)
 
 
-@dispatch(bcolz.carray, np.ndarray)
+@dispatch(carray, np.ndarray)
 def into(a, b, **kwargs):
-    return bcolz.carray(b, **kwargs)
+    return carray(b, **kwargs)
 
 
 def fix_len_string_filter(ser):
@@ -46,37 +47,37 @@ def fix_len_string_filter(ser):
         return np.asarray(ser)
 
 
-@dispatch(bcolz.ctable, DataFrame)
+@dispatch(ctable, DataFrame)
 def into(a, df, **kwargs):
-    return bcolz.ctable([fix_len_string_filter(df[c]) for c in df.columns],
+    return ctable([fix_len_string_filter(df[c]) for c in df.columns],
                       names=list(df.columns), **kwargs)
 
 
-@dispatch(bcolz.carray, (tuple, list))
+@dispatch(carray, (tuple, list))
 def into(a, b, **kwargs):
     x = into(np.ndarray(0), b)
     return into(a, x, **kwargs)
 
 
-@dispatch(bcolz.ctable, (tuple, list))
+@dispatch(ctable, (tuple, list))
 def into(a, b, **kwargs):
     if isinstance(b[0], (tuple, list)):
-        return bcolz.ctable([into(np.ndarray(0), c2) for c2 in zip(*b)], **kwargs)
+        return ctable([into(np.ndarray(0), c2) for c2 in zip(*b)], **kwargs)
     else:
-        return bcolz.ctable([into(np.ndarray(0), b)], **kwargs)
+        return ctable([into(np.ndarray(0), b)], **kwargs)
 
 
-@dispatch((bcolz.carray, bcolz.ctable), Iterator)
+@dispatch((carray, ctable), Iterator)
 def into(a, b, **kwargs):
     chunks = partition_all(1024, b)
     chunk = next(chunks)
-    a = bcolz.ctable([into(np.ndarray(0), c2) for c2 in zip(*chunk)], **kwargs)
+    a = ctable([into(np.ndarray(0), c2) for c2 in zip(*chunk)], **kwargs)
     for chunk in chunks:
         a.append(list(zip(*chunk)))
     a.flush()
     return a
 
-@dispatch(DataFrame, bcolz.ctable)
+@dispatch(DataFrame, ctable)
 def into(a, b, columns=None, schema=None):
     if not columns and schema:
         columns = dshape(schema)[0].names
@@ -88,11 +89,18 @@ def into(a, b, columns=None, schema=None):
 
 from .compute.chunks import ChunkIter
 
-@dispatch((bcolz.carray, bcolz.ctable), ChunkIter)
+@dispatch((carray, ctable), ChunkIter)
 def into(a, b, **kwargs):
     b = iter(b)
     a = into(a, next(b), **kwargs)
     for chunk in b:
         a.append(into(np.ndarray(0), chunk))
     a.flush()
+    return a
+
+
+from blaze.data.core import DataDescriptor
+@dispatch(DataDescriptor, (ctable, carray))
+def into(a, b, **kwargs):
+    a.extend_chunks(chunks(b))
     return a
